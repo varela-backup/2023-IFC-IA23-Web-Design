34e056b648676fa8512addc264503aead15850e6
Your Name <you@example.com>
2023-06-26 16:54:22 -0300

    ...

--- simon-ia23-turma1/main.js
+++ simon-ia23-turma1/main.js
@@ -1,5 +1,14 @@
-let bts = document.querySelectorAll("div.genius > *:not(.pontuacao)")
-let ordem = [0, 0, 1, 2, 1, 0, 1, 2]
+let genius = document.querySelector("div.genius")
+let bts = genius.querySelectorAll("*:not(.pontuacao)")
+let pontuacao = genius.querySelector(".pontuacao")
+let sequencia = [rng(), rng()]
+let currentIndex = 0
+let estado = "apresentando sequencia"
+let velocidade = 500
+
+function rng() {
+  return Math.floor(Math.random() * 4)
+}
 
 function ligar(item) {
   bts[item].classList.add("on")
@@ -11,5 +20,59 @@ function desligar(item) {
 
 function piscar(item) {
   ligar(item)
-  setTimeout(function () { desligar(item) }, 1000)
+  setTimeout(function () { desligar(item) }, velocidade)
 }
+
+function apresentarSequencia() {
+  let index = 0
+  let interval = null
+
+  return new Promise((resolve, reject) => {
+    function piscarCorAtual() {
+      if (index >= sequencia.length) {
+        clearInterval(interval)
+        resolve()
+        return
+      }
+      let atual = sequencia[index++]
+      piscar(atual)
+    }
+    interval = setInterval(piscarCorAtual, velocidade + 300)
+  })
+}
+
+genius.addEventListener("click", (ev) => {
+  if (estado != "jogando")
+    return
+
+  const buttonIndex = [...bts].indexOf(ev.target)
+
+  if (buttonIndex < 0)
+    return
+
+  if (buttonIndex != sequencia[currentIndex++]) {
+    estado = "derrota"
+    pontuacao.innerHTML = "PERDEU!"
+    return
+  }
+
+  if (currentIndex == sequencia.length) {
+    estado = "apresentando sequencia"
+    currentIndex = 0
+    iniciar()
+    return
+  }
+})
+
+async function iniciar() {
+  sequencia.push(rng())
+  if (estado == "apresentando sequencia") {
+    estado = "..."
+    pontuacao.innerHTML = "..."
+    await apresentarSequencia()
+    estado = "jogando"
+    pontuacao.innerHTML = "Jogue"
+  }
+}
+
+pontuacao.addEventListener("click", iniciar)
